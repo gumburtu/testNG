30d70c6e8aab4d1e9346cd64c46908b5c2514512
gumburtu <aslanseval1986@gmail.com>
2025-04-01 17:29:23 +0100

    V23

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -52,6 +52,6 @@ public class SearchTest {
         driver.quit();
         driver.close(); //action class kapatmasini yazacagiz.//action class kapatmasini yazacagiz.
         //action class kapatmasini yazacagiz.//action class kapatmasini yazacagiz.
-        //kapatma
+        //kapatmaya close
     }
 }
